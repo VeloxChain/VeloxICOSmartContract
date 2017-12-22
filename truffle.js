cd43bd45e7e70f3cce1c0f097373a1956d0aef26
Eric Bui <EricBui@ExSmart.co>
2017-12-23 00:20:10 +0800

    - commit by eric

--- truffle.js
+++ truffle.js
@@ -1,9 +1,15 @@
 module.exports = {
   networks: {
-    development: {
+      kovan: {
+          network_id: 42,    // Official kovan network id
+          host:'localhost',
+          port:8545,
+          gas: 4500000
+      }//,
+   /* development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    }*/
   }
 };
